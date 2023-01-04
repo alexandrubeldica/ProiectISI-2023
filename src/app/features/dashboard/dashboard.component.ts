d1bb1b1b0dc064411c1379b4dde049fcf076ab02
oanasavu <savu.oana09@gmail.com>
2023-01-05 00:34:12 +0200

    harta

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,22 +1,208 @@
-import { Component, OnInit } from '@angular/core';
+import {
+   Component,
+   OnInit,
+   ElementRef, 
+  ViewChild,
+  EventEmitter,
+  OnDestroy } from '@angular/core';
 
 import { AuthService } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { setDefaultOptions, loadModules } from 'esri-loader';
+import { Subscription } from "rxjs";
+import esri = __esri; // Esri TypeScript Types
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  @ViewChild("mapViewNode", { static: true }) private mapViewEl: ElementRef;
+
+  // register Dojo AMD dependencies
+  _Map;
+  _MapView;
+  _FeatureLayer;
+  _Graphic;
+  _GraphicsLayer;
+  _Route;
+  _RouteParameters;
+  _FeatureSet;
+  _Point;
+  _locator;
+
+  // Instances
+  map: esri.Map;
+  view: esri.MapView;
+  pointGraphic: esri.Graphic;
+  graphicsLayer: esri.GraphicsLayer;
+ 
+
+  // Attributes
+  zoom = 12;
+  center: Array<number> = [26.10253839,44.4267674];
+  basemap = "streets-vector";
+  loaded = false;
+  pointCoords: number[] = [26.10253839,44.4267674];
+  dir: number = 0;
+  count: number = 0;
+  timeoutHandler = null;
+
+  async initializeMap() {
+    try {
+      // configure esri-loader to use version x from the ArcGIS CDN
+      // setDefaultOptions({ version: '3.3.0', css: true });
+      setDefaultOptions({ css: true });
+
+      // Load the modules for the ArcGIS API for JavaScript
+      const [esriConfig, Map, MapView, FeatureLayer, Graphic, Point, GraphicsLayer] = await loadModules([
+        "esri/config",
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer",
+        "esri/Graphic",
+        "esri/geometry/Point",
+        "esri/layers/GraphicsLayer",
+      ]);
+
+      // esriConfig.apiKey = "MY_API_KEY";
+
+      this._Map = Map;
+      this._MapView = MapView;
+      this._FeatureLayer = FeatureLayer;
+      this._Graphic = Graphic;
+      this._GraphicsLayer = GraphicsLayer;
+      this._Point = Point;
+
+      esriConfig.apiKey = "AAPK3bb84377534f45308d3724b0ff5fc06al8ttIhyK2iQI9_x3xU_4zItzpcq56u99ddU2j2zJODpfc2abUERjNmfatJZuvyaz";
+
+      // Configure the Map
+      const mapProperties = {
+        basemap: this.basemap
+      };
+
+      this.map = new Map(mapProperties);
+
+      this.addFeatureLayers();
+      this.addGraphicLayers();
+
+      this.addPoint(this.pointCoords[1], this.pointCoords[0], true);
+
+      // Initialize the MapView
+      const mapViewProperties = {
+        container: this.mapViewEl.nativeElement,
+        center: this.center,
+        zoom: this.zoom,
+        map: this.map
+      };
+
+      this.view = new MapView(mapViewProperties);
+
+      // Fires `pointer-move` event when user clicks on "Shift"
+      // key and moves the pointer on the view.
+      this.view.on('pointer-move', ["Shift"], (event) => {
+        let point = this.view.toMap({ x: event.x, y: event.y });
+        console.log("map moved: ", point.longitude, point.latitude);
+      });
+
+      await this.view.when(); // wait for map to load
+      console.log("ArcGIS map loaded");
+      console.log("Map center: " + this.view.center.latitude + ", " + this.view.center.longitude);
+      return this.view;
+    } catch (error) {
+      console.log("EsriLoader: ", error);
+    }
+  }
+
+  addGraphicLayers() {
+    this.graphicsLayer = new this._GraphicsLayer();
+    this.map.add(this.graphicsLayer);
+  }
+
+  addFeatureLayers() {
+    // Trailheads feature layer (points)
+    var trailheadsLayer: __esri.FeatureLayer = new this._FeatureLayer({
+      url:
+        "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+    });
+
+    this.map.add(trailheadsLayer);
+
+    // Trails feature layer (lines)
+    var trailsLayer: __esri.FeatureLayer = new this._FeatureLayer({
+      url:
+        "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
+    });
+
+    this.map.add(trailsLayer, 0);
+
+    // Parks and open spaces (polygons)
+    var parksLayer: __esri.FeatureLayer = new this._FeatureLayer({
+      url:
+        "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
+    });
+
+    this.map.add(parksLayer, 0);
+
+    console.log("feature layers added");
+  }
+
+  addPoint(lat: number, lng: number, register: boolean) {   
+    const point = { //Create a point
+      type: "point",
+      longitude: lng,
+      latitude: lat
+    };
+    const simpleMarkerSymbol = {
+      type: "simple-marker",
+      color: [226, 119, 40],  // Orange
+      outline: {
+        color: [255, 255, 255], // White
+        width: 1
+      }
+    };
+    let pointGraphic: esri.Graphic = new this._Graphic({
+      geometry: point,
+      symbol: simpleMarkerSymbol
+    });
+
+    this.graphicsLayer.add(pointGraphic);
+    if (register) {
+      this.pointGraphic = pointGraphic;
+    }
+  }
+
+  removePoint() {
+    if (this.pointGraphic != null) {
+      this.graphicsLayer.remove(this.pointGraphic);
+    }
+  }
 
   logout() {
     this.authService
       .logout()
       .then(() => this.router.navigate(['/']))
       .catch((e) => console.log(e.message));
+  }
+
+  ngOnInit() {
+    // Initialize MapView and return an instance of MapView
+    console.log("initializing map");
+    this.initializeMap().then(() => {
+      // The map has been initialized
+      console.log("mapView ready: ", this.view.ready);
+      this.loaded = this.view.ready;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.view) {
+      // destroy the map view
+      this.view.container = null;
+    }
   }
 }
--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,4 +1,3 @@
-
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
